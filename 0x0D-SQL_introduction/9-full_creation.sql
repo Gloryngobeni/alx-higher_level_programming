fdb8741dad2a733341abbc908f6846ad122dabcc
Gloryngobeni <www.gloryngobeni@gmail.com>
2024-05-23 01:10:55 -0700

    sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++
@@ -1,6 +0,0 @@
--- Creating a second table.
-CREATE TABLE IF NOT EXISTS second_table(id int, name varchar(256), score int);
-INSERT INTO second_table(id, name, score) VALUES (1, "John", 10);
-INSERT INTO second_table(id, name, score) VALUES (2, "Alex", 3);
-INSERT INTO second_table(id, name, score) VALUES (3, "Bobo", 14);
-INSERT INTO second_table(id, name, score)  VALUES (4, "George", 8);
